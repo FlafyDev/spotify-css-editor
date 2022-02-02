ea64f10dc07a97ae3c14071a846e441f745aaeb6
FlafyDev <flafyarazi@gmail.com>
2022-02-02 22:45:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# spotify-css-editor
+# Spotify CSS Editor
 Edit CSS directly from the Spotify.
+
+Download from [Spicetify Marketplace](https://github.com/CharlieS1103/spicetify-marketplace).
---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import CSSEditor from './css-editor';
+import { SettingsSection } from 'spicetify-creator-settings-plugin';
+
+async function main() {
+  let mainViewContainer: HTMLElement | null = null;
+  while (true) {
+    mainViewContainer = document?.getElementById('main');
+
+    if (Spicetify?.Platform?.History?.listen && mainViewContainer) break;
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+
+  const cssEditorContainer = document.createElement('div');
+  mainViewContainer.appendChild(cssEditorContainer);
+  ReactDOM.render(<CSSEditor />, cssEditorContainer)
+}
+
+export default main;
